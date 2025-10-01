e219aa019d8634952d878dbefd01c74ae8a06dfa
Penalver-Ezequiel <ezepenalver29@gmail.com>
2025-10-01 20:26:56 -0300

    feat: Se agrego la Entidad Pedido Faltante

--- src/main/java/com/example/puntofacil/demo/entity/Pedido.java
+++ src/main/java/com/example/puntofacil/demo/entity/Pedido.java
@@ -1,5 +1,95 @@
 package com.example.puntofacil.demo.entity;
 
-public class Pedido {
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String codigoRetiro;
+    private String qrCodeUrl;
+    private LocalDateTime fecha;
+    private String estado; // PENDIENTE, PAGADO, PREPARANDO, etc.
+    private BigDecimal total;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<Pago> pagos;
+
+    public Pedido() {
+    }
+
+    public Pedido(Long id, String codigoRetiro, String qrCodeUrl, LocalDateTime fecha, String estado, BigDecimal total,
+            List<Pago> pagos) {
+        this.id = id;
+        this.codigoRetiro = codigoRetiro;
+        this.qrCodeUrl = qrCodeUrl;
+        this.fecha = fecha;
+        this.estado = estado;
+        this.total = total;
+        this.pagos = pagos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigoRetiro() {
+        return codigoRetiro;
+    }
+
+    public void setCodigoRetiro(String codigoRetiro) {
+        this.codigoRetiro = codigoRetiro;
+    }
+
+    public String getQrCodeUrl() {
+        return qrCodeUrl;
+    }
+
+    public void setQrCodeUrl(String qrCodeUrl) {
+        this.qrCodeUrl = qrCodeUrl;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public List<Pago> getPagos() {
+        return pagos;
+    }
+
+    public void setPagos(List<Pago> pagos) {
+        this.pagos = pagos;
+    }
+
+    
 }
